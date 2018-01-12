9788a39280cf8dc11cf298f3ee28bdf666edec73
github username <lina.rudashevski@gmail.com>
2018-01-11 20:48:51 -0500

    pagination working but needs to be fixed for large page numbers

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -1,19 +1,40 @@
 import React from 'react'
-import {Container,Table} from 'semantic-ui-react'
+import {Container,Table, Menu} from 'semantic-ui-react'
 import Transaction from './Transaction'
+let item
+let transactionsLength
 
 class TransactionsTable extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      currentPage: 1,
+      transactionsPerPage: 25,
+      activeItem: 1
+    }
+  }
+
+  handlePageChange = (event) => {
+    this.setState({currentPage: Number(event.target.id), activeItem: Number(event.target.id)})
+  }
 
   transactionData = () => {
+    const {currentPage, transactionsPerPage} = this.state
+    const indexOfLastTransaction = currentPage * transactionsPerPage
+    const indexofFirstTransaction = indexOfLastTransaction-transactionsPerPage
+
     if (this.props.account.length > 0) {
-      let item = this.props.account[0]
-      return item.transactions.map((transaction, index) => {
+      item = this.props.account[0].transactions.slice(indexofFirstTransaction,indexOfLastTransaction)
+      transactionsLength = this.props.account[0].transactions.length
+      return item.map((transaction, index) => {
         return (
           <Transaction key={index} transaction={transaction}/>
         )
       })
     } else {
-      return this.props.transactions.map((transaction,index) => {
+      item = this.props.transactions.slice(indexofFirstTransaction, indexOfLastTransaction)
+      transactionsLength = this.props.transactions.length
+      return item.map((transaction,index) => {
         return (
           <Transaction key={index} transaction={transaction}/>
         )
@@ -21,8 +42,25 @@ class TransactionsTable extends React.Component {
     }
   }
   render(){
-    console.log(this.props)
+    const {activeItem, transactionsPerPage} = this.state
     let data = this.transactionData()
+    let pageNumbers = []
+    for (let i = 1; i <=Math.ceil(transactionsLength/transactionsPerPage); i++) {
+      pageNumbers.push(i)
+    }
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <Menu.Item
+        id={number}
+        key={number}
+        active={activeItem === number}
+        onClick={this.handlePageChange}
+        >
+          {number}
+        </Menu.Item>
+      )
+    })
+
     return (
       <Container>
         <Table>
@@ -36,6 +74,15 @@ class TransactionsTable extends React.Component {
           <Table.Body>
             {data}
           </Table.Body>
+          <Table.Footer>
+            <Table.Row>
+              <Table.HeaderCell colSpan='7'>
+                <Menu floated='right' pagination>
+                  {renderPageNumbers}
+                </Menu>
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Footer>
         </Table>
       </Container>
     )
